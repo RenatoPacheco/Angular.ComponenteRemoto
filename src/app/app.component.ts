c4dbe9a6f9e73419a8bb7372d2642d230952f6d8
Renato Pacheco <renato.pacheco@outlook.com>
2021-03-07 12:44:51 -0300

    Incluindo js dos componentes remotos por script, para posteriormente usar variaveis da ambiente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Objetivo-Portal-Componente';
+
+  ngOnInit(): void {
+    const cabecalho = document.createElement('script');
+    cabecalho.src = 'http://localhost:8080/cabecalho/main.js';
+    document.body.appendChild(cabecalho);
+
+    const rodape = document.createElement('script');
+    rodape.src = 'http://localhost:8080/rodape/main.js';
+    document.body.appendChild(rodape);
+  }
 }
